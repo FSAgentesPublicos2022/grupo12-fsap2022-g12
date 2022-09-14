b0930f04c29ee197cdb952fde7a21057358be410
Silvia <silviagev@gmail.com>
2022-09-14 20:52:14 -0300

    se creo componente Transacciones

--- src/app/historicos/historicos.module.ts
+++ src/app/historicos/historicos.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TransaccionesComponent } from './Transacciones/Transacciones.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
     CommonModule
   ]
