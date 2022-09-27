ff1e9e09183e98923843b19f1f67ae9f12e75ad5
romansad <romansad@gmail.com>
2022-09-27 19:50:37 -0300

    Carpeta Models

---
+++ 3-back/back_wallet/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace back_wallet.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string NombreUser { get; set; }
+        public string Contrasenia { get; set; }
+        public Boolean IsAdmin { get; set; }
+        public int? IdPersona { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public DateTime FechaBaja { get; set; }
+
+    }
+}
