d1ffd99a48700656150ece909de903119ee87e37
Andres B <andresblasiche@gmail.com>
2022-10-20 19:32:02 -0300

    Formulario de registro

--- 2-front/src/app/gestion-users/registro/registro.component.ts
+++ 2-front/src/app/gestion-users/registro/registro.component.ts
@@ -1,4 +1,5 @@
   import { Component, OnInit } from '@angular/core';
+  import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
   @Component({
     selector: 'registro',
@@ -6,10 +7,58 @@
     styleUrls: ['./registro.component.css']
   })
   export class RegistroComponent implements OnInit {
-  
-    constructor() { }
-  
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    registroForm = this.formBuilder.group(
+      {
+        nombre: ["", Validators.required],
+        apellido: ["", Validators.required],
+        email: ["", [Validators.required, Validators.email]],
+        clave: ["", [Validators.required, Validators.minLength(8)]],
+        rptClave: ["", Validators.required]
+      },
+      {
+        validator: this.validarClaves("clave", "rptClave")
+      }
+    );
+
     ngOnInit(): void {
     }
   
+    get form() {
+      return this.registroForm.controls;
+    }
+
+    onReset() {
+      this.registroForm.reset();
+    }
+
+    onSubmit() {
+      if (this.registroForm.invalid) {
+        return;
+      }
+  
+      alert(
+        "SUCCESS!! :-)\n\n" + JSON.stringify(this.registroForm.value, null, 4)
+      );
+    }
+
+    validarClaves(ctrlClave: string, ctrlRptClave: string) {
+      return (formGroup: FormGroup) => {
+        const clave = formGroup.controls[ctrlClave];
+        const claveControl = formGroup.controls[ctrlRptClave];
+  
+        if (claveControl.errors && !claveControl.errors['validarClaves']) {
+          return;
+        }
+  
+        if (clave.value !== claveControl.value) {
+          claveControl.setErrors({ validarClaves: true });
+        } else {
+          claveControl.setErrors(null);
+        }
+      };
+    }
+
   }
